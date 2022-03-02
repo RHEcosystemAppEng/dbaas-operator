6d734700a0d6c841b539df98b1ff976c579552f7
Ritesh Chikatwar <rchikatw@redhat.com>
2022-03-02 12:39:25 +0530

    Adding handler

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/go-logr/logr v0.4.0
+	github.com/oklog/run v1.1.0
 	github.com/onsi/ginkgo v1.16.4
 	github.com/onsi/gomega v1.13.0
 	github.com/openshift/api v0.0.0-20210910062324-a41d3573a3ba
--- metrics/main.go
+++ metrics/main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"net"
+	"net/http"
+	"strconv"
+
+	"github.com/RHEcosystemAppEng/dbaas-operator/metrics/internal/handler"
+	"github.com/oklog/run"
+	"github.com/prometheus/client_golang/prometheus"
+	"k8s.io/klog"
+)
+
+const (
+	host                      = "0.0.0.0"
+	customResourceMetricsPort = 8080
+	exporterMetricsPort       = 8081
+)
+
 func main() {
 
+	exporterRegistry := prometheus.NewRegistry()
+	// Add exporter self metrics collectors to the registry.
+
+	// serves exporter self metrics
+	exporterMux := http.NewServeMux()
+	handler.RegisterExporterMuxHandlers(exporterMux, exporterRegistry)
+
+	customResourceMux := http.NewServeMux()
+	handler.RegisterExporterMuxHandlers(exporterMux, exporterRegistry)
+
+	var rg run.Group
+	rg.Add(listenAndServe(exporterMux, host, exporterMetricsPort))
+	rg.Add(listenAndServe(customResourceMux, host, customResourceMetricsPort))
+
+}
+
+func listenAndServe(mux *http.ServeMux, host string, port int) (func() error, func(error)) {
+	var listener net.Listener
+	serve := func() error {
+		addr := net.JoinHostPort(host, strconv.Itoa(port))
+		listener, err := net.Listen("tcp", addr)
+		if err != nil {
+			return err
+		}
+		return http.Serve(listener, mux)
+	}
+	cleanup := func(error) {
+		err := listener.Close()
+		if err != nil {
+			klog.Errorf("failed to close listener: %v", err)
+		}
+	}
+	return serve, cleanup
 }
