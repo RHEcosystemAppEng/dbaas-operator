0684942c234fdadbcae7ff39ded225e6173f1a8a
Abdul Hameed <ahameed@redhat.com>
2022-04-26 16:28:03 -0400

    added metrics and observability changes
    getting the cluster_id and setting into observability cr, to show into observatorium
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    enabling the metrics configuration for the operator
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    added metric for duration of DBaaS platform stack installation
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    added metric for duration of DBaaS platform stack installation
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    expose the metrics port from the pod and service to allow the Service Monitor to read from it
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>
    
    Add ServiceMonitor and MonitoringStackOperator
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>
    
    added metrics for inventory, connections, instance status
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    metrics cleanups on deletion of inventory, connection, instance
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    Update MSO(observability operator)
    
    1. Update the version from 0.0.8 to 0.0.11
    2. Update the name of the operator from monitoring stack to observability
    3. Install opertor like other provider operators
    4. Rename Type Provider to type Operator
    4. Rename Type Provider to type Operator
    4. Rename Type Provider to type Operator
    4. Rename Type Provider to type Operator
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>
    
    added metric for duration of DBaaS platform stack installation
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    added metrics for inventory, connections, instance status
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    metrics cleanups on deletion of inventory, connection, instance
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    WIP: Additional metrics from ADR completed
    
    enabling the metrics configuration for the operator
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    added metric for duration of DBaaS platform stack installation
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    added metric for duration of DBaaS platform stack installation
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    expose the metrics port from the pod and service to allow the Service Monitor to read from it
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>
    
    Add ServiceMonitor and MonitoringStackOperator
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>
    
    added metrics for inventory, connections, instance status
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    metrics cleanups on deletion of inventory, connection, instance
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    WIP: Additional metrics from ADR completed
    
    WIP:rebased and removed mso-catalog0source-subscription.yaml
    
    creating the observability operator MonitoringStack CR using reconciler once the observability operator installed.
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    code refactor and fix the request duration metrics
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>
    
    Update Observability Opertor version
    
    Signed-off-by: Rewant Soni <resoni@redhat.com>
    
    observability rebase fixes
    
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>
    
    PR Review fixes
    
    Signed-off-by: Abdul Hameed <ahameed@redhat.com>

---
+++ controllers/event_handler.go
@@ -0,0 +1,38 @@
+package controllers
+
+import (
+	"go.uber.org/zap"
+	k8serrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+)
+
+// EventHandlerWithDelete is an extension of EnqueueRequestForObject that will _not_ trigger a reconciliation for a Delete event.
+// Instead, it will call an external controller's Delete() method and pass the event argument unchanged.
+type EventHandlerWithDelete struct {
+	handler.EnqueueRequestForObject
+	Controller interface {
+		Delete(e event.DeleteEvent) error
+	}
+}
+
+func (d *EventHandlerWithDelete) Delete(e event.DeleteEvent, _ workqueue.RateLimitingInterface) {
+	objectKey := ObjectKeyFromObject(e.Object)
+	log := zap.S().With("resource", objectKey)
+
+	if err := d.Controller.Delete(e); err != nil && k8serrors.IsNotFound(err) {
+		log.Errorf("Object (%s) removed from Kubernetes, but controller could not delete it: %s", e.Object.GetObjectKind(), err)
+	}
+}
+
+func ObjectKeyFromObject(obj metav1.Object) client.ObjectKey {
+	return ObjectKey(obj.GetNamespace(), obj.GetName())
+}
+
+func ObjectKey(namespace, name string) client.ObjectKey {
+	return types.NamespacedName{Name: name, Namespace: namespace}
+}
