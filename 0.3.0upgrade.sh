a864f63772fe2293e9f59c924fc1b74f84efc245
Tommy Hughes <tohughes@redhat.com>
2022-10-17 11:34:02 -0500

    addon fixes
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- 0.3.0upgrade.sh
+++ 0.3.0upgrade.sh
@@ -12,7 +12,7 @@ fi
 verlt() {
     requiredVer="4.9"
     ocVer=$(oc version --client | awk -F : {'print $2'} | tr -d "[:space:]")
-    if [ -z ${ocVer} ]; then
+    if [ -z "${ocVer}" ]; then
         return 0
     fi
     if [ "${ocVer}" == "${requiredVer}" ]; then
@@ -49,7 +49,7 @@ fi
 installns02=$(oc get csv dbaas-operator.v0.2.0 --ignore-not-found -o template --template '{{index .metadata.annotations "olm.operatorNamespace"}}')
 installns03=$(oc get csv dbaas-operator.v0.3.0 --ignore-not-found -o template --template '{{index .metadata.annotations "olm.operatorNamespace"}}')
 
-if [ ! -z ${installns02} ] && [ ! -z ${installns03} ]; then
+if [ ! -z "${installns02}" ] && [ ! -z "${installns03}" ]; then
     echo "Running script against ${installns02} project"
 
     ocNsPerms="get subscriptions.operators.coreos.com,patch subscriptions.operators.coreos.com,create subscriptions.operators.coreos.com,delete subscriptions.operators.coreos.com,get deploy,patch deploy,delete dbaasplatform,delete csv"
@@ -67,15 +67,15 @@ if [ ! -z ${installns02} ] && [ ! -z ${installns03} ]; then
     done
 
     subname=$(oc get subscriptions.operators.coreos.com addon-dbaas-operator -n ${installns02} --ignore-not-found --template '{{.metadata.name}}')
-    if [ -z ${subname} ]; then
+    if [ -z "${subname}" ]; then
         subname=$(oc get subscriptions.operators.coreos.com dbaas-operator -n ${installns02} --ignore-not-found --template '{{.metadata.name}}')
     fi
 
-    if [ ! -z ${subname} ]; then
+    if [ ! -z "${subname}" ]; then
         echo ""
         # add if check to see if manager exists first
         deploy=$(oc get deploy dbaas-operator-controller-manager -n ${installns02} --ignore-not-found --template '{{.metadata.name}}')
-        if [ ! -z ${deploy} ]; then
+        if [ ! -z "${deploy}" ]; then
             # stop 0.2.0 operator
             oc scale --replicas=0 deploy dbaas-operator-controller-manager -n ${installns02}
             sleep 3
@@ -84,7 +84,7 @@ if [ ! -z ${installns02} ] && [ ! -z ${installns03} ]; then
         function restoreDeploy()
         {
           echo "Interrupt caught, scaling controller manager & exiting..."
-          if [ ! -z ${deploy} ]; then
+          if [ ! -z "${deploy}" ]; then
                 oc scale --replicas=1 deploy dbaas-operator-controller-manager -n ${installns02}
           fi
           exit 1
@@ -132,12 +132,10 @@ EOF
         sleep 3
 
         echo ""
-        echo "Removing RHODA 0.2.0 platform CR"
+        echo "Removing RHODA 0.2.0 platform CR(s)"
         oc delete dbaasplatform dbaas-platform --ignore-not-found --wait -n ${installns02}
         if [ $(oc auth can-i delete dbaasplatforms --all-namespaces) == "yes" ]; then
-            echo ""
-            echo "Removing RHODA 0.2.0 dbaasplatform CRD"
-            oc delete dbaasplatforms --all --all-namespaces --ignore-not-found --wait
+            oc delete dbaasplatform --all --all-namespaces --ignore-not-found --wait
         fi
 
         # upgrade should succeed regardless, but will attempt to remove the offending crd
@@ -159,14 +157,20 @@ EOF
             dbaas-operator.v0.3.0 \
             --ignore-not-found --wait -n ${installns02}
 
-        addon=$(oc get addon dbaas-operator --ignore-not-found --template '{{.metadata.name}}')
-        addonSub=$(oc get subscriptions.operators.coreos.com addon-dbaas-operator -n ${installns02} --ignore-not-found --template '{{.metadata.name}}')
-        if [ ! -z ${addon} ] && [ ! -z ${addonSub} ]; then
-            oc delete subscriptions.operators.coreos.com ${addonSub} --ignore-not-found --wait -n ${installns02}
-            sleep 5
+        addonSub=""
+        addonsApi=$(oc api-resources --api-group=addons.managed.openshift.io --no-headers --namespaced=false -o name)
+        if [ ! -z "${addonsApi}" ]; then
+            if [ $(oc auth can-i get addons --all-namespaces) == "yes" ]; then
+                addon=$(oc get addon dbaas-operator --ignore-not-found --template '{{.metadata.name}}')
+                addonSub=$(oc get subscriptions.operators.coreos.com addon-dbaas-operator -n ${installns02} --ignore-not-found --template '{{.metadata.name}}')
+                if [ ! -z "${addon}" ] && [ ! -z "${addonSub}" ]; then
+                    oc delete subscriptions.operators.coreos.com ${addonSub} --ignore-not-found --wait -n ${installns02}
+                    sleep 5
+                fi
+            fi
         fi
-        addonSubcheck=$(oc get subscriptions.operators.coreos.com addon-dbaas-operator -n ${installns02} --ignore-not-found --template '{{.metadata.name}}')
-        if [ -z ${addonSubcheck} ]; then
+
+        if [ -z "${addonSub}" ]; then
             cat <<EOF | oc create -f -
 ${subscription}
 EOF
