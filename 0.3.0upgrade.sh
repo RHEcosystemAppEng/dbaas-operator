511d6d26d8d2213764c00ee653f49868f327dc8f
xiezhang <xiezhang@redhat.com>
2022-10-18 01:06:21 -0400

    upgrade dbaasinstance CRs for the changes to status.phase

--- 0.3.0upgrade.sh
+++ 0.3.0upgrade.sh
@@ -2,9 +2,9 @@
 # this should be run if RHODA gets stuck in "pending" when attempting to upgrade from 0.2.0 -> 0.3.0
 set -e
 
-# require oc client version 4.9 or greater
+# require oc client version 4.11 or greater
 verlt () {
-    requiredVer="4.9"
+    requiredVer="4.11"
     ocVer=$(oc version --client | awk -F : {'print $2'} | tr -d "[:space:]")
     if [ -z "${ocVer}" ]; then
         return 0
@@ -13,6 +13,22 @@ verlt () {
         return 1
     fi
     if [  "${ocVer}" == "`echo -e "${ocVer}\n${requiredVer}" | sort -V | head -n1`" ]; then
+        return 0
+    fi
+    return 1
+}
+
+# require jq version 1.5 or greater
+verjq() {
+    jqRequiredVer="jq-1.5"
+    jqVer=$(jq --version)
+    if [ -z "${jqVer}" ]; then
+        return 0
+    fi
+    if [ "${jqVer}" == "${jqRequiredVer}" ]; then
+        return 1
+    fi
+    if [  "${jqVer}" == "`echo -e "${jqVer}\n${jqRequiredVer}" | sort -V | head -n1`" ]; then
         return 0
     fi
     return 1
@@ -31,6 +47,22 @@ cliChecks () {
         echo "Currently running ${ocVer}"
         echo ""
         echo 'upgrade and try again - https://docs.okd.io/latest/cli_reference/openshift_cli/getting-started-cli.html'
+        echo ""
+        exit
+    fi
+
+    if ! which jq > /dev/null; then
+       echo "'jq' command not found"
+       echo 'install the jq 1.5+ and try again - https://stedolan.github.io/jq/download/'
+       exit
+    fi
+
+    if verjq; then
+        echo ""
+        echo "This script requires 'jq' version ${jqRequiredVer} or greater."
+        echo "Currently running ${jqVer}"
+        echo ""
+        echo 'upgrade and try again - https://stedolan.github.io/jq/download/'
         echo ""
         exit
     fi
@@ -86,6 +118,24 @@ deletePlatformCRD () {
     fi
 }
 
+upgradeInstanceCRs () {
+   echo ""
+   echo "Upgrading RHODA 0.2.0 dbaasinstance CR(s)"
+   SUPPORTED_PHASES="[\"Unknown\",\"Pending\",\"Creating\",\"Updating\",\"Deleting\",\"Deleted\",\"Ready\",\"Error\",\"Failed\"]"
+   for insCRD in $INS_CRDS; do
+       dbaasinstances=$(oc get $insCRD --all-namespaces -o=json | jq --argjson PHASES "$SUPPORTED_PHASES" '.items[] | select(.status.phase as $p | $PHASES | all(.!=$p)) | "\(.metadata.name),\(.metadata.namespace)"')
+       if [ ! -z "${dbaasinstances}" ]; then
+           saveIFS="$IFS"
+           for ins in $dbaasinstances; do
+               nsn=${ins:1:-1}
+               IFS=, read -r name namespace <<< $nsn
+               oc patch $insCRD $name -n $namespace --subresource='status' --type='merge' -p '{"status":{"phase":"Unknown"}}'
+           done
+           IFS="$saveIFS"  # Set IFS back to normal!
+       fi
+   done
+}
+
 deleteOldCSVs () {
     echo ""
     echo "Removing RHODA 0.2.0 CSVs"
@@ -105,6 +155,7 @@ cleanupPriorRelease () {
     deleteSubs
     deletePlatformCRs
     deletePlatformCRD
+    upgradeInstanceCRs
     deleteOldCSVs
 }
 
@@ -133,6 +184,26 @@ if [ ! -z "${installns02}" ] && [ ! -z "${installns03}" ]; then
         else
             echo "user cannot '${nsPerm}' in the ${installns02} project"
             echo "'oc login ...' with a user that has admin rights to the ${installns02} project and try again"
+            exit
+        fi
+    done
+
+    INS_CRDS="dbaasinstance crdbdbaasinstance crunchybridgeinstance mongodbatlasinstance rdsinstance"
+    for insCRD in $INS_CRDS; do
+        if [ $(oc auth can-i get $insCRD --all-namespaces) == "yes" ]; then
+            echo -n "."
+        else
+            echo "user cannot get ${insCRD}"
+            echo "'oc login ...' with a user that has admin rights to get ${insCRD} and try again"
+            exit
+        fi
+    done
+    for insCRD in $INS_CRDS; do
+        if [ $(oc auth can-i patch $insCRD --subresource=status --all-namespaces) == "yes" ]; then
+            echo -n "."
+        else
+            echo "user cannot patch ${insCRD} status subresource"
+            echo "'oc login ...' with a user that has admin rights to patch ${insCRD} status subresource and try again"
             exit
         fi
     done
